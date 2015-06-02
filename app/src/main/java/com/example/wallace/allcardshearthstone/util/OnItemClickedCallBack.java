21b6b530abfc01fd9e9c4a732ca10f9708702620
Wallace Luis do Carmo filho <ninja_krexi@hotmail.com>
2015-06-02 03:29:38 -0300

    up

---
+++ app/src/main/java/com/example/wallace/allcardshearthstone/util/OnItemClickedCallBack.java
@@ -0,0 +1,8 @@
+package com.example.wallace.allcardshearthstone.util;
+
+/**
+ * Created by Wallace on 02/06/2015.
+ */
+public interface OnItemClickedCallBack {
+    public void onItemClickedCallBack (int position);
+}
