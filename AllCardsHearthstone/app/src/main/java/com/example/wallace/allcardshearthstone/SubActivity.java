b3b5e2e799b0b6c5c684e13c0a983f64b249b5d6
Wallace Luis do Carmo filho <ninja_krexi@hotmail.com>
2015-06-03 18:12:02 -0300

    Navegação da Aplicação

--- AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivity.java
+++ AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivity.java
@@ -1,36 +1,34 @@
 package com.example.wallace.allcardshearthstone;
 
-import android.app.Fragment;
-import android.content.Intent;
-import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.Toolbar;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SubActivity extends ActionBarActivity {
 
     private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
         toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sub, menu);
         return true;
     }
 
@@ -43,16 +41,10 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Toast.makeText(this, "Hey you just hit " + item.getTitle(), Toast.LENGTH_SHORT).show();
             return true;
         }
-
-        if (id == R.id.action_cards) {
-            startActivity(new Intent(this, SubActivity.class));
-        }
-
-        if (id == R.id.action_blizz) {
-            startActivity(new Intent(this, SubActivityBlizz.class));
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
         }
 
         return super.onOptionsItemSelected(item);
--- AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivityBlizz.java
+++ AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivityBlizz.java
@@ -1,36 +1,32 @@
 package com.example.wallace.allcardshearthstone;
 
-import android.app.Fragment;
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SubActivityBlizz extends ActionBarActivity {
 
     private android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub_activity_blizz);
 
         toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sub_activity_blizz, menu);
         return true;
     }
 
@@ -43,16 +39,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Toast.makeText(this, "Hey you just hit " + item.getTitle(), Toast.LENGTH_SHORT).show();
             return true;
-        }
-
-        if (id == R.id.action_cards) {
-            startActivity(new Intent(this, SubActivity.class));
-        }
-
-        if (id == R.id.action_blizz) {
-            startActivity(new Intent(this, SubActivityBlizz.class));
         }
 
         return super.onOptionsItemSelected(item);
