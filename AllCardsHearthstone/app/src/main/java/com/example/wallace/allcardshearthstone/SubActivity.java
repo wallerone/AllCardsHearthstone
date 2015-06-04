4329146e5ab90e689fa470a1e6dc8946437ae109
Wallace Luis do Carmo filho <ninja_krexi@hotmail.com>
2015-06-04 02:57:54 -0300

    Ajustes

--- AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivity.java
+++ AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivity.java
@@ -41,7 +41,6 @@ public class SubActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sub, menu);
         return true;
     }
 
--- AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivityBlizz.java
+++ AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivityBlizz.java
@@ -26,7 +26,6 @@ public class SubActivityBlizz extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sub_activity_blizz, menu);
         return true;
     }
 
