a857b87e184cacd9d23b49b58d903c9326710914
Wallace Luis do Carmo filho <ninja_krexi@hotmail.com>
2015-06-06 06:49:05 -0300

    Fragment... Navigation Drawer

--- AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/MainActivity.java
+++ AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/MainActivity.java
@@ -1,35 +1,39 @@
 package com.example.wallace.allcardshearthstone;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private android.support.v7.widget.Toolbar toolbar;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
+
     }
 
-    public void getJson(View view){
+    public void getJson(View view) {
 
     }
-
 
 
     @Override
--- AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivity.java
+++ AllCardsHearthstone/app/src/main/java/com/example/wallace/allcardshearthstone/SubActivity.java
@@ -1,32 +1,31 @@
 package com.example.wallace.allcardshearthstone;
 
 import android.support.v4.app.NavUtils;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toolbar;
 
 
 public class SubActivity extends ActionBarActivity {
 
     private RecyclerView recyclerView;
-    private android.support.v7.widget.Toolbar toolbar;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
-        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.app_bar);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 //    @Override
